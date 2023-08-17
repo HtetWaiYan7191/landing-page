d32c206d6f5d7cdb6546944f2f2c09f3153dee42
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-17 21:16:27 +0630

    Add basic structure navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../styles/Navbar.css'
+import {BiLockOpen} from 'react-icons/bi'
+
+const Navbar = () => {
+  return (
+    <nav id='navbar-wrapper' className='navbar-section border-b fixed top-0 left-0 w-[100%]'>
+        <div className="navbar-container flex justify-around items-center py-4">
+            <h2 className='text-3xl text-green-500 font-semibold'>Practice</h2>
+            <ul className='navbar-list flex'>
+                <li className='me-2 text-sm text-green-500'>Home</li>
+                <li className='me-2 text-sm'>About</li>
+                <li className='me-2 text-sm'>Course</li>
+                <li className='me-2 text-sm'>Blog</li>
+                <li className='me-2 text-sm'>Contact</li>
+            </ul>
+            <div className='login-container flex items-center'>
+                <div className='lock-icon  flex items-center me-4'>
+                    <BiLockOpen className='me-2'/>
+                    <span>Login</span>
+                </div>
+                <button className=' bg-green-600 text-white p-2 px-3 rounded-md'>Sign up for Free</button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
