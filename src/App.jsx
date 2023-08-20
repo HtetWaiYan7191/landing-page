61acdd97c25efce99b5cf74a3e07f5d8e14f29bc
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-20 13:29:52 +0630

    Add style for slick slider components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Introduction from './components/Introduction'
 import Navbar from './components/Navbar'
 import Course from './components/Course'
+import '../src/App.css'
 const App = () => {
   return (
    <>
