0fd1a9b6ede42a519793c79a29d94ae6b7e1f692
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-23 20:15:21 +0630

    Add required images for categories section

---
+++ src/assets/categoriesInfo/categories.js
@@ -0,0 +1,77 @@
+import { penNib, fileHtml, microphoneStage, briefCase, sunHorizon, camera, musicNote, database, lightBulb, heartBeat, graph, detective} from '../categoriesInfo/imgExport';
+
+const categories = [
+    {
+        id:1,
+        name: 'Design',
+        logoImage:penNib,
+    },
+ 
+    {
+        id:2,
+        name: 'Development',
+        logoImage:fileHtml,
+    },
+
+    {
+        id:3,
+        name: 'Marketing',
+        logoImage:microphoneStage,
+    },
+
+    {
+        id:4,
+        name: 'Business',
+        logoImage:briefCase,
+    },
+
+    {
+        id:5,
+        name: 'Lifestyle',
+        logoImage:sunHorizon,
+    },
+
+    {
+        id:6,
+        name: 'Photography',
+        logoImage:camera,
+    },
+
+    {
+        id:7,
+        name: 'Music',
+        logoImage:musicNote,
+    },
+
+    {
+        id:8,
+        name: 'Data Science',
+        logoImage:database,
+    },
+
+    {
+        id:9,
+        name: 'Personal Develop',
+        logoImage:lightBulb,
+    },
+
+    {
+        id:10,
+        name: 'Health & Fitness',
+        logoImage:heartBeat,
+    },
+
+    {
+        id:11,
+        name: 'Finance',
+        logoImage:graph,
+    },
+
+    {
+        id:12,
+        name: 'Teaching',
+        logoImage:detective,
+    },
+];
+
+export default categories
---
+++ src/assets/categoriesInfo/imgExport.js
@@ -0,0 +1,14 @@
+import penNib from './categoriesImage/penNib.png';
+import fileHtml from './categoriesImage/FileHtml.png';
+import microphoneStage from './categoriesImage/MicrophoneStage.png';
+import briefCase from './categoriesImage/Briefcase.png';
+import sunHorizon from './categoriesImage/SunHorizon.png';
+import camera from './categoriesImage/Camera (1).png';
+import musicNote from './categoriesImage/MusicNote.png';
+import database from './categoriesImage/Database.png'
+import lightBulb from './categoriesImage/Lightbulb.png';
+import heartBeat from './categoriesImage/Heartbeat.png';
+import graph from './categoriesImage/Graph.png';
+import detective from './categoriesImage/Detective.png';
+
+export {penNib, fileHtml, microphoneStage, briefCase, sunHorizon, camera, musicNote, database, lightBulb, heartBeat, graph, detective}
