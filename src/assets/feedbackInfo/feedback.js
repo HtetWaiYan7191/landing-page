09ea6ec957036b8c88cda360adb6cb91b1d43f9d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-23 21:30:10 +0630

    create array to create a feedback carousel

---
+++ src/assets/feedbackInfo/feedback.js
@@ -0,0 +1,39 @@
+import boy from './boy.png';
+import girl from './girl.png';
+
+const feedback = [
+    {
+        id: 1,
+        name: 'Jenny Wilson',
+        job: 'UI-UX Designer',
+        description: 'Ut pharetra ipsum nec leo blandit, sit amet tincidunt eros pharetra. Nam sed imperdiet turpis. In hac habitasse platea dictumst. Praesent nulla massa, hendrerit vestibulum gravida in, feugiat auctor felis.',
+        profileImg:girl,
+    },
+
+    {
+        id: 2,
+        name: 'Guy Hawkins',
+        job: 'Front End Developer',
+        description: 'Ut pharetra ipsum nec leo blandit, sit amet tincidunt eros pharetra. Nam sed imperdiet turpis. In hac habitasse platea dictumst. Praesent nulla massa, hendrerit vestibulum gravida in, feugiat auctor felis.',
+        profileImg:boy,
+    },
+
+    {
+        id: 3,
+        name: 'Happy Boy',
+        job: 'Back End Developer',
+        description: 'Ut pharetra ipsum nec leo blandit, sit amet tincidunt eros pharetra. Nam sed imperdiet turpis. In hac habitasse platea dictumst. Praesent nulla massa, hendrerit vestibulum gravida in, feugiat auctor felis.',
+        profileImg:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    },
+
+    {
+        id: 4,
+        name: 'Hinata',
+        job: 'Full Stack Developer',
+        description: 'Ut pharetra ipsum nec leo blandit, sit amet tincidunt eros pharetra. Nam sed imperdiet turpis. In hac habitasse platea dictumst. Praesent nulla massa, hendrerit vestibulum gravida in, feugiat auctor felis.',
+        profileImg:"https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    }
+
+]
+
+export default feedback;
