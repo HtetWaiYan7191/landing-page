fd71b072b33db4331bb829f1e1aec356d801365a
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-24 20:52:34 +0630

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import '../styles/Navbar.css'
 import {BiLockOpen} from 'react-icons/bi'
 
 const Navbar = () => {
+
+    useEffect(() => {
+        const sections = document.querySelectorAll('section');
+        const navItems = document.querySelectorAll('.navbar-list li');
+    
+        const updateActiveNavItem = () => {
+          const scrollPosition = window.scrollY;
+    
+          sections.forEach((section, index) => {
+            const topOffset = section.offsetTop;
+            const bottomOffset = topOffset + section.clientHeight;
+    
+            if (scrollPosition >= topOffset && scrollPosition < bottomOffset) {
+              navItems.forEach(navItem => {
+                navItem.classList.remove('activeNavItem');
+              });
+              navItems[index].classList.add('activeNavItem');
+            }
+          });
+        };
+    
+        window.addEventListener('scroll', updateActiveNavItem);
+    
+        return () => {
+          window.removeEventListener('scroll', updateActiveNavItem);
+        };
+      }, []);
   return (
     <nav id='navbar-wrapper' className='navbar-section border-b fixed top-0 left-0 w-[100%] z-10 backdrop-blur-sm bg-white/80'>
         <div className="navbar-container flex justify-around items-center py-4">
             <h2 className='text-3xl text-green-500 font-semibold'>Practice</h2>
             <ul className='navbar-list flex'>
-                <li className='me-2 text-sm text-green-500'>Home</li>
-                <li className='me-2 text-sm'>About</li>
-                <li className='me-2 text-sm'>Course</li>
-                <li className='me-2 text-sm'>Blog</li>
-                <li className='me-2 text-sm'>Contact</li>
+               <a href="#introduction-wrapper"> <li className='me-2 text-sm active:text-green-700 hover:text-green-500 activeNavItem'>Home</li></a>
+               <a href="#course-wrapper"> <li className='me-2 text-sm active:text-green-700 hover:text-green-500 activeNavItem'>Course</li></a>
+               <a href="#achievement-wrapper"> <li className='me-2 text-sm active:text-green-700 hover:text-green-500 activeNavItem'>About</li></a>
+               <a href="#feedback-wrapper"> <li className='me-2 text-sm active:text-green-700 hover:text-green-500 activeNavItem'>Feedback</li></a>
+               <a href="#register-wrapper"> <li className='me-2 text-sm active:text-green-700 hover:text-green-500 activeNavItem'>Contact</li></a>
             </ul>
             <div className='login-container flex items-center'>
                 <div className='lock-icon  flex items-center me-4'>
