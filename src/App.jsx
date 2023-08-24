67fd17d7b34972678c34f1aa4848b19d0ab58566
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-24 19:47:59 +0630

    Add register section in App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import '../src/App.css'
 import Achievement from './components/Achievement'
 import Categories from './components/Categories'
 import Feedback from './components/Feedback'
+import Register from './components/Register'
 const App = () => {
   return (
    <>
@@ -15,6 +16,7 @@ const App = () => {
     <Achievement/>
     <Categories/>
     <Feedback/>
+    <Register/>
    </>
   )
 }
